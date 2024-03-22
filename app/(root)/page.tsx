52ee87bbc4ea55704e3b3e1e152b3e1d9c0a4e8e
RobelRH <robel.redda28@gmail.com>
2024-03-22 07:27:40 +0300

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,56 @@
+import { Collection } from '@/components/shared/Collection'
+import { navLinks } from '@/constants'
+import { getAllImages } from '@/lib/actions/image.actions'
+import { UserButton } from '@clerk/nextjs'
+import { link } from 'fs'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Home = async ({ searchParams } : SearchParamProps  ) => {
+
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
+
+  const images = await getAllImages({ page, searchQuery });
+
+  return (
+    <>
+      <section className='home'>
+        <h1 className='home-heading py-5'>
+          Genius GPT
+        </h1>
+        <ul className='flex flex-center w-full gap-4'>
+          {
+            navLinks
+            .slice(1, 5).map((link) => (
+              <Link
+                key={link.route}
+                href={link.route}
+                className='flex-center flex-row gap-1 bg-[#343839] p-3 rounded-lg hover:bg-[#141718]'
+              >
+                <li className='flex-center w-fit'>
+                  <Image src={link.icon} alt="image" width={24} height={24} />
+                </li>
+                <li className='p-14-medium text-center text-white'>
+                  <p>{link.label}</p>
+                </li>
+              </Link>
+            ))
+          }
+        </ul>
+      </section>
+
+      <section className='sm:mt-12'>
+          <Collection
+           hasSearch={true}
+           images={images?.data} 
+           totalPages={images?.totalPage}
+           page={page} />
+      </section>
+
+    </>
+  )
+}
+
+export default Home
