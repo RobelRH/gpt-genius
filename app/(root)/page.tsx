319f3ad6ad77b8625924423993e9b023ef68a89e
RobelRH <robel.redda28@gmail.com>
2024-03-22 20:29:14 +0300

    styling customized

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,46 +7,100 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Home = async ({ searchParams } : SearchParamProps  ) => {
+import { LayoutDashboard, MessageSquare, ImageIcon, Code, Settings } from "lucide-react"
+
+const Home = async ({ searchParams }: SearchParamProps) => {
 
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
 
   const images = await getAllImages({ page, searchQuery });
 
+  const navLinks = [
+    {
+      label: "Home",
+      route: "/",
+      icon: LayoutDashboard,
+      color: "text-sky-500"
+    },
+    {
+      label: "Image Restore",
+      route: "/transform/add/restore",
+      icon: MessageSquare,
+      color: "text-green-500"
+    },
+    {
+      label: "Generative Fill",
+      route: "/transform/add/fill",
+      icon: ImageIcon,
+      color: "text-orange-500"
+    },
+    {
+      label: "Object Remove",
+      route: "/transform/add/remove",
+      icon: Code,
+      color: "text-blue-500"
+    },
+    {
+      label: "Object Recolor",
+      route: "/transform/add/recolor",
+      icon: Settings,
+      color: "text-purple-500"
+    },
+    {
+      label: "Background Remove",
+      route: "/transform/add/removeBackground",
+      icon: ImageIcon,
+      color: "text-gray-500"
+    },
+    {
+      label: "Profile",
+      route: "/profile",
+      icon: MessageSquare,
+      color: "text-pink-500"
+    },
+    {
+      label: "Buy Credits",
+      route: "/credits",
+      icon: Code,
+      color: "text-red-500"
+    },
+  ];
+
   return (
     <>
       <section className='home'>
-        <h1 className='home-heading py-5'>
+        <h1 className='home-heading py-5 '>
           Genius GPT
         </h1>
         <ul className='flex flex-center w-full gap-4'>
           {
             navLinks
-            .slice(1, 5).map((link) => (
-              <Link
-                key={link.route}
-                href={link.route}
-                className='flex-center flex-row gap-2 bg-[#343839] p-3 rounded-lg hover:bg-[#141718]'
-              >
-                <li className='flex-center w-fit'>
-                  <Image src={link.icon} alt="image" width={18} height={18} />
-                </li>
-                <li className='text-sm text-center text-white'>
-                  <p>{link.label}</p>
-                </li>
-              </Link>
-            ))
+              .slice(1, 5).map((link) => (
+                <Link
+                  key={link.route}
+                  href={link.route}
+                  className='flex-center flex-row gap-2 bg-[#343839] p-3 rounded-lg hover:bg-[#141718]'
+                >
+                  <li className='flex-center w-fit'>
+                    {/* <Image src={link.icon} alt="image" width={18} height={18} /> */}
+                    <link.icon className={link.color} />
+                  </li>
+                  <li className='text-sm text-center text-white'>
+                    <p>{link.label}</p>
+                  </li>
+                </Link>
+              ))
           }
         </ul>
       </section>
 
       <section className='sm:mt-1'>
-          <Collection
-           hasSearch={true}
-           images={images?.data} 
-           totalPages={images?.totalPage}
-           page={page} />
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page} />
       </section>
 
     </>
--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,23 +1,74 @@
 "use client"
 
-import { navLinks } from '@/constants'
+// import { navLinks } from '@/constants'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import { Button } from '../ui/button'
-import { LayoutDashboard, MessageSquare, ImageIcon, Music, VideoIcon, Code, Settings } from "lucide-react"
+import { LayoutDashboard, MessageSquare, ImageIcon, Code, Settings } from "lucide-react"
 
 const Sidebar = () => {
 
   const pathname = usePathname();
 
+  const navLinks = [
+    {
+      label: "Home",
+      route: "/",
+      icon: LayoutDashboard,
+      color: "text-sky-500"
+    },
+    {
+      label: "Image Restore",
+      route: "/transform/add/restore",
+      icon: MessageSquare,
+      color: "text-green-500"
+    },
+    {
+      label: "Generative Fill",
+      route: "/transform/add/fill",
+      icon: ImageIcon,
+      color: "text-orange-500"
+    },
+    {
+      label: "Object Remove",
+      route: "/transform/add/remove",
+      icon: Code,
+      color: "text-blue-500"
+    },
+    {
+      label: "Object Recolor",
+      route: "/transform/add/recolor",
+      icon: Settings,
+      color: "text-purple-500"
+    },
+    {
+      label: "Background Remove",
+      route: "/transform/add/removeBackground",
+      icon: ImageIcon,
+      color: "text-gray-500"
+    },
+    {
+      label: "Profile",
+      route: "/profile",
+      icon: MessageSquare,
+      color: "text-pink-500"
+    },
+    {
+      label: "Buy Credits",
+      route: "/credits",
+      icon: Code,
+      color: "text-red-500"
+    },
+  ];
+
   return (
     <aside className='sidebar'>
       <div className='flex size-full flex-col gap-4'>
-        <Link href="/" className='sidebar-logo'>
-          <Image src="/assets/images/logo-text.svg" alt="logo" width={18} height={28} />
+        <Link href="/" className='sidebar-logo ml-3'>
+          <Image src="/assets/images/logo-text.svg" alt="logo" width={200} height={50} />
         </Link>
 
         <nav className='sidebar-nav'>
@@ -30,14 +81,7 @@ const Sidebar = () => {
                   <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-[#232627] text-white' : 'text-white'
                     }`}>
                     <Link className='sidebar-link' href={link.route}>
-                      {/* <Image
-                        src={link.icon}
-                        alt='logo'
-                        width={22}
-                        height={20}
-                        className={`${isActive} && 'brightness-200' bg-orange-700 text-green-500`}
-                      /> */}
-                      <Code color='blue' />
+                      <link.icon  className={link.color} />
                       {link.label}
                     </Link>
                   </li>
@@ -54,13 +98,7 @@ const Sidebar = () => {
                   <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-[#232627] text-white' : 'text-white'
                     }`}>
                     <Link className='sidebar-link' href={link.route}>
-                      <Image
-                        src={link.icon}
-                        alt='logo'
-                        width={24}
-                        height={24}
-                        className={`${isActive} && 'brightness-200'`}
-                      />
+                      <link.icon className={link.color} />
                       {link.label}
                     </Link>
                   </li>
